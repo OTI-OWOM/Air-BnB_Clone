3bdf28becc7ee3ccb6e1e9837d8bab44586e4e47
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-11-17 22:43:27 +0100

    documentations

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,21 @@ class BaseModel:
     """Defines a simple base class that other model can inherit from"""
 
     def __init__(self):
+        """Initialize a new BaseModel instance"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """Return string representation of BaseModel instance"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """Update updated_at with current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Return dictionary representation of BaseModel instance"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
 

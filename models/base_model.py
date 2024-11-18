0b9843eb60d90f3682d2e5c3836d31f88d19efae
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-11-18 05:10:00 +0100

    tasks 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,21 @@ from datetime import datetime
 class BaseModel:
     """Defines a simple base class that other model can inherit from"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, time_format))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return string representation of BaseModel instance"""
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -35,6 +35,15 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(model_dict["created_at"], model.created_at.isoformat())
         self.assertEqual(model_dict["updated_at"], model.updated_at.isoformat())
 
+    def test_to_dict_methods(self):
+        """Test the to_dict method returns correct dictionary representation."""
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertEqual(model_dict["__class__"], "BaseModel")
+        self.assertEqual(model_dict["id"], model.id)
+        self.assertEqual(model_dict["created_at"], model.created_at.isoformat())
+        self.assertEqual(model_dict["updated_at"], model.updated_at.isoformat())
+
 
 if __name__ == "__main__":
     unittest.main()

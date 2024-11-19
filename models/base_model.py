12bd91efb4c4ca163bb41f2808d3d930baf76e9b
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-11-19 15:15:37 +0100

    updated to use the storage instance to store and retrieve json contents

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This module contains the base Model"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -12,16 +13,20 @@ class BaseModel:
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
 
         if kwargs:
+            # If created from a dictionary (e.g., from JSON)
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
+                        # Converts string time stamp to datetime
                         setattr(self, key, datetime.strptime(value, time_format))
                     else:
                         setattr(self, key, value)
         else:
+            # New Instance creation
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Return string representation of BaseModel instance"""
@@ -30,6 +35,7 @@ class BaseModel:
     def save(self):
         """Update updated_at with current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Return dictionary representation of BaseModel instance"""

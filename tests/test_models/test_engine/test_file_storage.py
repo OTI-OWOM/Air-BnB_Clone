3683f9663597198ae1bfa3dd47214bd648fcb409
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-11-19 16:18:24 +0100

    file_path tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,40 +1,8 @@
-#!/usr/bin/python3
-"""Defines unittest for model/engine/file_storage.py.
-"""
+import unittest
 import os
 import json
-import models
-import unittest
-from datetime import datetime
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-
-
-class TestFilesStorage_instantiation(unittest.TestCase):
-    """Unittest for testing the instantiation of the FileStorage class."""
-    
-    def test_FileStorage_instantiation_no_args(self):
-        self.assertEqual(type(FileStorage()), FileStorage)
-
-    def test_FileStorage_instantiation_with_args(self):
-        with self.assertRaises(TypeError):
-            FileStorage(None)
-
-    def test_FileStorage_file_path_is_private_str(self):
-        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
-
-    def test_FileStorage_objects_is_private_dict(self):
-        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
-
-    def test_storage_initializes(self):
-        self.assertEqual(type(models.storage), FileStorage)
-
-    def test_file_path(self):
-        self.assertIsInstance(FileStorage._FileStorage__file_path, str,
-                              "File path must be a string")
-        self.assertTrue(FileStorage._FileStorage__file_path.endswith('.json'),
-                        "File path must end with a json")
-
 
 class TestFileStorage(unittest.TestCase):
     def setUp(self):

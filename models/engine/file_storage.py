5f089c1bddfb05fc164f302938743222ae0ada8f
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-11-20 04:29:47 +0100

    models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,7 +7,7 @@ from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
 from models.review import Review
-from models.state import state
+from models.state import State
 from models.user import User
 
 
@@ -43,7 +43,7 @@ class FileStorage:
         classes = {
                 'Amenity': Amenity,
                 'BaseModel': BaseModel,
-                'City',: City,
+                'City': City,
                 'Place': Place,
                 'Review': Review,
                 'State': State,
